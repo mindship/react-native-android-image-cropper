2361b35b3023a8caace9cb7344b679fb5ea22287
Anish Kargaonkar <anishkargaonkar@gmail.com>
2019-11-22 14:01:05 +0530

    add support for passing image file URI to cropper directly

--- android/src/main/java/com/ozdevcode/theartofdev/edmodo/utils/ResponseHelper.java
+++ android/src/main/java/com/ozdevcode/theartofdev/edmodo/utils/ResponseHelper.java
@@ -1,6 +1,6 @@
 package com.ozdevcode.theartofdev.edmodo.utils;
 
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
